cdd1cb3a10c796ff607a1721da49951f0c7bd8e2
Vladyslav Troian <vladyslav.troian@cyper.ca>
2022-06-24 18:14:53 +0300

    add useffects in the single task and styles

--- src/components/SingleListItem/SingleListItem.tsx
+++ src/components/SingleListItem/SingleListItem.tsx
@@ -1,24 +1,42 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import ListItem from "@mui/material/ListItem";
 import ListItemAvatar from "@mui/material/ListItemAvatar";
-import ListItemText from "@mui/material/ListItemText";
-import IconButton from "material-ui/IconButton";
+import IconButton from "@mui/material/IconButton";
 import Avatar from "@mui/material/Avatar";
 import EditIcon from "@mui/icons-material/Edit";
 import PushPinIcon from "@mui/icons-material/PushPin";
 import DeleteIcon from "@mui/icons-material/Delete";
 import AssignmentIcon from "@mui/icons-material/Assignment";
+import SaveIcon from "@mui/icons-material/Save";
+import Input from "@mui/material/Input";
+import "./SingleListItem.scss";
+import Box from "@mui/material/Box";
 
-export function SingleListItem() {
+interface SingleListItemProps {
+  text: string;
+  tags: string;
+}
+
+export function SingleListItem({ text, tags }: SingleListItemProps) {
+  const [editableText, setEditableText] = useState(true);
+  const [pinnedTask, setPinnedTask] = useState(false);
+
   return (
     <ListItem
+      className="list__item"
       secondaryAction={
         <>
-          <IconButton aria-label="pin">
+          <IconButton
+            aria-label="pin"
+            color={pinnedTask ? "primary" : "default"}
+            onClick={() => setPinnedTask((state) => !state)}>
             <PushPinIcon />
           </IconButton>
-          <IconButton aria-label="edit">
+          <IconButton aria-label="edit" onClick={() => setEditableText(false)}>
             <EditIcon />
+          </IconButton>
+          <IconButton aria-label="save" onClick={() => setEditableText(true)}>
+            <SaveIcon />
           </IconButton>
           <IconButton aria-label="delete">
             <DeleteIcon />
@@ -26,11 +44,14 @@ export function SingleListItem() {
         </>
       }>
       <ListItemAvatar>
-        <Avatar>
-          <AssignmentIcon />
+        <Avatar variant="rounded">
+          <AssignmentIcon color={pinnedTask ? "primary" : undefined} />
         </Avatar>
       </ListItemAvatar>
-      <ListItemText primary="Single-line item" secondary="#main #test #view" />
+      <Box className="list__item-inputs">
+        <Input className="list__item-input" multiline disabled={editableText} defaultValue={text} fullWidth />
+        <Input className="list__item-input" multiline disabled={editableText} defaultValue={tags} fullWidth />
+      </Box>
     </ListItem>
   );
 }
--- src/components/TaskList/TaskList.tsx
+++ src/components/TaskList/TaskList.tsx
@@ -1,14 +1,7 @@
+/* eslint-disable no-irregular-whitespace */
 import Box from "@mui/material/Box";
 import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemAvatar from "@mui/material/ListItemAvatar";
-import ListItemText from "@mui/material/ListItemText";
-import Avatar from "@mui/material/Avatar";
-import IconButton from "@mui/material/IconButton";
-import EditIcon from "@mui/icons-material/Edit";
-import PushPinIcon from "@mui/icons-material/PushPin";
-import DeleteIcon from "@mui/icons-material/Delete";
-import AssignmentIcon from "@mui/icons-material/Assignment";
+import { SingleListItem } from "../SingleListItem";
 import "./TaskList.scss";
 
 export function TaskList() {
@@ -16,74 +9,16 @@ export function TaskList() {
     <Box className="container">
       <Box>
         <List>
-          <ListItem
-            className="list__item"
-            secondaryAction={
-              <>
-                <IconButton aria-label="pin">
-                  <PushPinIcon />
-                </IconButton>
-                <IconButton aria-label="edit">
-                  <EditIcon />
-                </IconButton>
-                <IconButton aria-label="delete">
-                  <DeleteIcon />
-                </IconButton>
-              </>
-            }>
-            <ListItemAvatar>
-              <Avatar>
-                <AssignmentIcon />
-              </Avatar>
-            </ListItemAvatar>
-            <ListItemText primary="Single-line item" secondary="#main #test #view" />
-          </ListItem>
-          <ListItem
-            className="list__item"
-            secondaryAction={
-              <>
-                <IconButton aria-label="pin">
-                  <PushPinIcon />
-                </IconButton>
-                <IconButton aria-label="edit">
-                  <EditIcon />
-                </IconButton>
-                <IconButton aria-label="delete">
-                  <DeleteIcon />
-                </IconButton>
-              </>
-            }>
-            <ListItemAvatar>
-              <Avatar>
-                <AssignmentIcon />
-              </Avatar>
-            </ListItemAvatar>
-            <ListItemText primary="Single-line item" secondary="#main #test #view" />
-          </ListItem>
-          <ListItem
-            className="list__item"
-            secondaryAction={
-              <>
-                <IconButton aria-label="pin">
-                  <PushPinIcon />
-                </IconButton>
-                <IconButton aria-label="edit">
-                  <EditIcon />
-                </IconButton>
-                <IconButton aria-label="delete">
-                  <DeleteIcon />
-                </IconButton>
-              </>
-            }>
-            <ListItemAvatar>
-              <Avatar>
-                <AssignmentIcon />
-              </Avatar>
-            </ListItemAvatar>
-            <ListItemText primary="Single-line item" secondary="#main #test #view" />
-          </ListItem>
+          <SingleListItem text="Single-line item" tags="#main #test #view" />
+          <SingleListItem text="Single-line item" tags="#main #test #view" />
+          <SingleListItem text="Single-line item" tags="#main #test #view" />
         </List>
       </Box>
     </Box>
   );
 }
+
+// "SingleListItem" невозможно использовать как компонент JSX.
+// eslint-disable-next-line no-irregular-whitespace
+//   Тип возвращаемого значения "ReactNode" не является допустимым элементом JSX.
+//     Тип "undefined" не может быть назначен для типа "Element | null".
