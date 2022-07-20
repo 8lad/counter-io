ef54c156bede438d2afcf50d0937a6f2932606cb
Vladyslav Troian <vladyslav.troian@cyper.ca>
2022-07-20 11:22:38 +0300

    added redux devtools and updated hotring files

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -4,6 +4,7 @@ import { rootReducer } from "./rootReducer";
 export const store = configureStore({
   reducer: rootReducer,
   middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
+  devTools: true,
 });
 
 export type RootState = ReturnType<typeof store.getState>;
